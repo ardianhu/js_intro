3c9becf3cd2e1602e1926b9031084c49c98d9362
ardianhu <118362672+ardianhu@users.noreply.github.com>
2023-12-11 15:41:44 +0700

    finished
    back-end for beginner completed, with 5 starts :D

--- notes-app/src/server.js
+++ notes-app/src/server.js
@@ -1,7 +1,10 @@
 const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
 
 const init = async () => {
-  const server = Hapi.server({ prot: 5000, host: 'localhost' })
+  const server = Hapi.server({ port: 8000, host: 'localhost', routes: { cors: { origin: ['*'] } } })
+
+  server.route(routes)
 
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
